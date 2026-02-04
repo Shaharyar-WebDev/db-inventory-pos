b27f246c688cc74b89e78cd7ff49c88235940713
Shaharyar Ahmed <shaharyar@DESKTOP-U23GP1D.localdomain>
2026-02-05 02:43:42 +0500

    Override default loading indicator

--- app/Support/helpers.php
+++ app/Support/helpers.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Str;
 use App\Models\Scopes\OutletScope;
+use Illuminate\Support\HtmlString;
+use Filament\Support\Enums\IconSize;
+use Illuminate\Contracts\Support\Htmlable;
+use Illuminate\View\ComponentAttributeBag;
 
 if (! function_exists('app_currency_symbol')) {
     function app_currency_symbol()
@@ -55,4 +59,18 @@ if (! function_exists('default_number_format')) {
     {
         return number_format($value);
     }
+}
+
+function generate_loading_indicator_html(?ComponentAttributeBag $attributes = null, ?IconSize $size = null): Htmlable
+{
+    $size ??= IconSize::Medium;
+
+    $attributes = ($attributes ?? new ComponentAttributeBag)->class([
+        'fi-icon fi-loading-indicator',
+        "fi-size-{$size->value}",
+    ]);
+
+    return new HtmlString(<<<HTML
+            <svg {$attributes->toHtml()} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-loader-icon lucide-loader"><path d="M12 2v4"/><path d="m16.2 7.8 2.9-2.9"/><path d="M18 12h4"/><path d="m16.2 16.2 2.9 2.9"/><path d="M12 18v4"/><path d="m4.9 19.1 2.9-2.9"/><path d="M2 12h4"/><path d="m4.9 4.9 2.9 2.9"/></svg>
+            HTML);
 }
