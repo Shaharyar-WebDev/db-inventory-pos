3fd619f6665b4b5e0d5c695419ee8e8cb2e6c27e
Shaharyar Ahmed <shaharyar@DESKTOP-U23GP1D.localdomain>
2026-02-05 23:27:34 +0500

    removed infolist code to use form as view page

--- app/Filament/Outlet/Resources/Sale/Sales/Schemas/SaleInfolist.php
+++ app/Filament/Outlet/Resources/Sale/Sales/Schemas/SaleInfolist.php
@@ -12,32 +12,32 @@ class SaleInfolist
     {
         return $schema
             ->components([
-                TextEntry::make('sale_number'),
-                TextEntry::make('customer.name')
-                    ->label('Customer'),
-                TextEntry::make('description')
-                    ->placeholder('-')
-                    ->columnSpanFull(),
-                TextEntry::make('total')
-                    ->numeric(),
-                TextEntry::make('discount_type')
-                    ->badge(),
-                TextEntry::make('discount_value')
-                    ->numeric(),
-                TextEntry::make('grand_total')
-                    ->numeric(),
-                TextEntry::make('outlet.name')
-                    ->label('Outlet')
-                    ->placeholder('-'),
-                TextEntry::make('created_at')
-                    ->dateTime()
-                    ->placeholder('-'),
-                TextEntry::make('updated_at')
-                    ->dateTime()
-                    ->placeholder('-'),
-                TextEntry::make('deleted_at')
-                    ->dateTime()
-                    ->visible(fn (Sale $record): bool => $record->trashed()),
+                // TextEntry::make('sale_number'),
+                // TextEntry::make('customer.name')
+                //     ->label('Customer'),
+                // TextEntry::make('description')
+                //     ->placeholder('-')
+                //     ->columnSpanFull(),
+                // TextEntry::make('total')
+                //     ->numeric(),
+                // TextEntry::make('discount_type')
+                //     ->badge(),
+                // TextEntry::make('discount_value')
+                //     ->numeric(),
+                // TextEntry::make('grand_total')
+                //     ->numeric(),
+                // TextEntry::make('outlet.name')
+                //     ->label('Outlet')
+                //     ->placeholder('-'),
+                // TextEntry::make('created_at')
+                //     ->dateTime()
+                //     ->placeholder('-'),
+                // TextEntry::make('updated_at')
+                //     ->dateTime()
+                //     ->placeholder('-'),
+                // TextEntry::make('deleted_at')
+                //     ->dateTime()
+                //     ->visible(fn (Sale $record): bool => $record->trashed()),
             ]);
     }
 }
