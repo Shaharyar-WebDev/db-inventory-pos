11270f1ef32779904ca7aad52c9e582df663ac98
Shaharyar Ahmed <shaharyar@DESKTOP-U23GP1D.localdomain>
2026-02-26 00:10:11 +0500

    Disabled Settings for push

--- app/Settings/GeneralSettings.php
+++ app/Settings/GeneralSettings.php
@@ -25,9 +25,9 @@ class GeneralSettings extends Settings
 
     public string|Width|null $content_width;
 
-    public ?string $invoice_watermark_logo;
+    // public ?string $invoice_watermark_logo;
 
-    public ?string $invoice_footer_logo;
+    // public ?string $invoice_footer_logo;
 
     public static function group(): string
     {
