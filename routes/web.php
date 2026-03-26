e3e1741199b08328be74b7dc8b7742fc138ab670
Shaharyar Ahmed <shaharyar@DESKTOP-U23GP1D.localdomain>
2026-03-26 22:43:59 +0500

    Fixed optimize issue in settings

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,18 @@
 <?php
 
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
 
-// Route::get('/optimize', function () {
-//     Artisan::call('optimize:clear');
-//     Artisan::call('optimize');
+Route::get('/optimize', function () {
+    Artisan::call('optimize:clear');
+    Artisan::call('optimize');
 
-//     return back();
-// })->name('optimize');
+    return back();
+})->name('optimize');
 
-// Route::get('/optimize/clear', function () {
-//     $output = Artisan::call('optimize:clear');
+Route::get('/optimize/clear', function () {
+    $output = Artisan::call('optimize:clear');
 
-//     return back();
-// })->name('optimize:clear');
+    return back();
+})->name('optimize:clear');
