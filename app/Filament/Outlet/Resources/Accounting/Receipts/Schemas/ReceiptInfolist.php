04f97adcf6725a8ddf061151e25edcde5fb2b611
Shaharyar Ahmed <shaharyar@DESKTOP-U23GP1D.localdomain>
2026-04-01 00:00:31 +0500

    Fixed Sale InfoList Error

--- app/Filament/Outlet/Resources/Accounting/Receipts/Schemas/ReceiptInfolist.php
+++ app/Filament/Outlet/Resources/Accounting/Receipts/Schemas/ReceiptInfolist.php
@@ -12,27 +12,27 @@ class ReceiptInfolist
     {
         return $schema
             ->components([
-                TextEntry::make('customer_id')
-                    ->numeric(),
-                TextEntry::make('account_id')
-                    ->numeric(),
-                TextEntry::make('amount')
-                    ->numeric(),
-                TextEntry::make('remarks')
-                    ->placeholder('-')
-                    ->columnSpanFull(),
-                TextEntry::make('outlet.name')
-                    ->label('Outlet')
-                    ->placeholder('-'),
-                TextEntry::make('created_at')
-                    ->dateTime()
-                    ->placeholder('-'),
-                TextEntry::make('updated_at')
-                    ->dateTime()
-                    ->placeholder('-'),
-                TextEntry::make('deleted_at')
-                    ->dateTime()
-                    ->visible(fn (Receipt $record): bool => $record->trashed()),
+                // TextEntry::make('customer_id')
+                //     ->numeric(),
+                // TextEntry::make('account_id')
+                //     ->numeric(),
+                // TextEntry::make('amount')
+                //     ->numeric(),
+                // TextEntry::make('remarks')
+                //     ->placeholder('-')
+                //     ->columnSpanFull(),
+                // TextEntry::make('outlet.name')
+                //     ->label('Outlet')
+                //     ->placeholder('-'),
+                // TextEntry::make('created_at')
+                //     ->dateTime()
+                //     ->placeholder('-'),
+                // TextEntry::make('updated_at')
+                //     ->dateTime()
+                //     ->placeholder('-'),
+                // TextEntry::make('deleted_at')
+                //     ->dateTime()
+                //     ->visible(fn (Receipt $record): bool => $record->trashed()),
             ]);
     }
 }
