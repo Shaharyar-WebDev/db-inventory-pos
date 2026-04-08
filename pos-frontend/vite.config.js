9bcb415cffbd642374aef786b3bb3d34a626d7d0
Shaharyar Ahmed <shaharyar@DESKTOP-U23GP1D.localdomain>
2026-04-08 15:46:54 +0500

    Altered pos

--- pos-frontend/vite.config.js
+++ pos-frontend/vite.config.js
@@ -10,31 +10,16 @@ export default defineConfig({
     plugins: [
         tailwindcss(),
         vue(),
-        // VitePWA({
-        //     registerType: "autoUpdate",
-        //     workbox: {
-        //         globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
-        //         navigateFallback: "/pos-terminal/index.html",
-        //     },
-        //     manifest: {
-        //         name: "POS",
-        //         short_name: "POS",
-        //         start_url: "/pos-terminal/",
-        //         display: "standalone",
-        //         background_color: "#ffffff",
-        //         theme_color: "#000000",
-        //     },
-        // }),
         VitePWA({
             registerType: "autoUpdate",
             workbox: {
                 globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
-                navigateFallback: "/terminal/index.html", // ✅ public URL, not build folder
+                navigateFallback: "/pos-terminal/index.html",
             },
             manifest: {
                 name: "POS",
                 short_name: "POS",
-                start_url: "/terminal/",  // ✅ public URL, not build folder
+                start_url: "/pos-terminal/",
                 display: "standalone",
                 background_color: "#ffffff",
                 theme_color: "#000000",
