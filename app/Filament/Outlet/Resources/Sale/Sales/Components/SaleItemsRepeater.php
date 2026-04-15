95eab81fcbc08d134bd05e20c23ce2012906d69b
Shaharyar Ahmed <shaharyar@DESKTOP-U23GP1D.localdomain>
2026-04-15 23:11:23 +0500

    Fixed Product Select

--- app/Filament/Outlet/Resources/Sale/Sales/Components/SaleItemsRepeater.php
+++ app/Filament/Outlet/Resources/Sale/Sales/Components/SaleItemsRepeater.php
@@ -45,9 +45,9 @@ class SaleItemsRepeater
                         'name',
                         modifyQueryUsing: fn(Builder $query, $search) => $query
                             ->where('name', 'like', "%{$search}%")
-                            ->orWhereHas('parent', fn($q) => $q->where('name', 'like', "%{$search}%"))
+                            // ->orWhereHas('parent', fn($q) => $q->where('name', 'like', "%{$search}%"))
                             ->orWhereHas('brand', fn($q) => $q->where('name', 'like', "%{$search}%"))
-                            ->orWhereHas('unit', fn($q) => $q->where('name', 'like', "%{$search}%"))
+                            // ->orWhereHas('unit', fn($q) => $q->where('name', 'like', "%{$search}%"))
                             // ->orWhereHas('unit', fn($q) => $q->where('symbol', 'like', "%{$search}%"))
                             // ->orWhereHas('category', fn($q) => $q->where('name', 'like', "%{$search}%"))
                     )
