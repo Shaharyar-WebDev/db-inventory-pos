45da8fafba60d9f2bb1488b7febc49c47d2cae0b
Shaharyar Ahmed <shaharyar@DESKTOP-U23GP1D.localdomain>
2026-05-07 00:59:04 +0500

    Fixed Customer Rate relation manager

--- app/Filament/Admin/Resources/Master/Customers/RelationManagers/ProductRatesRelationManager.php
+++ app/Filament/Admin/Resources/Master/Customers/RelationManagers/ProductRatesRelationManager.php
@@ -2,7 +2,6 @@
 namespace App\Filament\Admin\Resources\Master\Customers\RelationManagers;
 
 use App\Models\Master\Product;
-use Filament\Actions\AssociateAction;
 use Filament\Actions\BulkActionGroup;
 use Filament\Actions\CreateAction;
 use Filament\Actions\DeleteAction;
@@ -15,6 +14,7 @@ use Filament\Schemas\Components\Utilities\Get;
 use Filament\Schemas\Schema;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Validation\Rule;
 
 class ProductRatesRelationManager extends RelationManager
@@ -27,6 +27,22 @@ class ProductRatesRelationManager extends RelationManager
             ->components([
                 Select::make('product_id')
                     ->relationship('product', 'name')
+                     ->getSearchResultsUsing(function (?string $search) {
+                $query = Product::query()
+                    ->where(function ($q) use ($search) {
+                        return $q->where('name', 'like', "%{$search}%")
+                            ->orWhereHas('parent', fn($q) => $q->where('name', 'like', "%{$search}%"))
+                            ->orWhereHas('brand', fn($q) => $q->where('name', 'like', "%{$search}%"))
+                            ->orWhereHas('unit', fn($q) => $q->where('name', 'like', "%{$search}%")->orWhere('symbol', 'like', "%{$search}%"))
+                            ->orWhereHas('category', fn($q) => $q->where('name', 'like', "%{$search}%"));
+                    })
+                    ->limit(10)
+                    ->get()
+                    ->mapWithKeys(fn($product) => [$product->id => $product->full_name]);
+
+                return $query;
+            })
+                    ->getOptionLabelFromRecordUsing(fn(Model $record) => $record->full_name)
                     ->live()
                     ->partiallyRenderComponentsAfterStateUpdated(['selling_price'])
                     ->rule(function ($record) {
